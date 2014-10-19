dd7d82c2a27f27d1cdf7a1722b792a2ca555df81
ggm <godgunman@gmail.com>
2014-10-19 16:38:38 +0800

    parse save and query

--- src/com/example/demoapp/MainActivity.java
+++ src/com/example/demoapp/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.demoapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.parse.FindCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -9,14 +16,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	private EditText editText;
 	private Button button;
+	private ListView listView;
+	private ProgressBar progressBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +40,9 @@ public class MainActivity extends Activity {
 
 		editText = (EditText) findViewById(R.id.editText1);
 		button = (Button) findViewById(R.id.button1);
-
+		listView = (ListView) findViewById(R.id.listView1);
+		progressBar	= (ProgressBar) findViewById(R.id.progressBar1);
+		
 		button.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -39,9 +53,44 @@ public class MainActivity extends Activity {
 
 				ParseObject testObject = new ParseObject("Message");
 				testObject.put("text", text);
-				testObject.saveInBackground();
+				testObject.saveInBackground(new SaveCallback() {
+					
+					@Override
+					public void done(ParseException e) {
+						loadDataFromParse();
+					}
+				});
 			}
 		});
+
+		loadDataFromParse();
+	}
+
+	private void loadDataFromParse() {
+		listView.setVisibility(View.INVISIBLE);
+		progressBar.setVisibility(View.VISIBLE);
+		
+		ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Message");
+		query.findInBackground(new FindCallback<ParseObject>() {
+
+			@Override
+			public void done(List<ParseObject> objects, ParseException e) {
+				progressBar.setVisibility(View.GONE);
+				listView.setVisibility(View.VISIBLE);
+				
+				List<String> data = new ArrayList<String>();
+				for (ParseObject object : objects) {
+					String text =object.getString("text");
+					data.add(text);
+				}
+				ArrayAdapter<String> adapter = 
+						new ArrayAdapter<String>(MainActivity.this, 
+								android.R.layout.simple_list_item_1, 
+								data);
+				listView.setAdapter(adapter);
+			}
+		});
+
 	}
 
 	@Override
