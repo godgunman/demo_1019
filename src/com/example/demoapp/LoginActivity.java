9513614d2a65ceb75e844bcf7fd993eb87768a49
ggm <godgunman@gmail.com>
2014-10-19 17:40:06 +0800

    enter to MainActivity

--- src/com/example/demoapp/LoginActivity.java
+++ src/com/example/demoapp/LoginActivity.java
@@ -3,8 +3,10 @@ package com.example.demoapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -24,14 +26,31 @@ public class LoginActivity extends Activity {
 		button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				String nickname = editText.getText().toString();
-				
-				Intent intent = new Intent();
-				intent.setClass(LoginActivity.this, 
-						MainActivity.class);
-				intent.putExtra("nickname", nickname);
-				startActivity(intent);
+				go();
 			}
 		});
+
+		editText.setOnKeyListener(new OnKeyListener() {
+
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (event.getAction() == KeyEvent.ACTION_DOWN
+						&& keyCode == KeyEvent.KEYCODE_ENTER) {
+					go();
+					return true;
+				}
+
+				return false;
+			}
+		});
+	}
+
+	private void go() {
+		String nickname = editText.getText().toString();
+
+		Intent intent = new Intent();
+		intent.setClass(LoginActivity.this, MainActivity.class);
+		intent.putExtra("nickname", nickname);
+		startActivity(intent);
 	}
 }
